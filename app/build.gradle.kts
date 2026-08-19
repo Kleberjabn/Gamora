e801daf600535045e55526a55e23b3fcd9d172ed
Kleberjabn <kleberjabn@yahoo.com.br>
2026-08-19 15:44:31 -0300

    prepare GamoraVet 1.0.0 final Play Store package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,10 +14,10 @@ android {
     }
 
     defaultConfig {
-        applicationId = "br.com.gamoravet.s23test"
+        applicationId = "br.com.gamoravet.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0-s23"
+        versionCode = 10
+        versionName = "1.0.0"
     }
 }
