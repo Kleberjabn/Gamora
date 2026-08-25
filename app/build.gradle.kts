df41721bdfe28cb6bddac03b96adad75bf6e4ae8
Kleberjabn <kleberjabn@yahoo.com.br>
2026-08-25 18:06:42 -0300

    Incrementar versão para novo teste interno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "br.com.gamoravet.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.0"
+        versionCode = 12
+        versionName = "1.0.1"
     }
 
     buildTypes {
