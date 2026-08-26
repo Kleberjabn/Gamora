f3bdfe207c85d1e1fc01fc927aebf5d5bde701e0
Kleberjabn <kleberjabn@yahoo.com.br>
2026-08-26 11:05:52 -0300

    Preparar versão 15 para teste interno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "br.com.gamoravet.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.2"
+        versionCode = 15
+        versionName = "1.0.3"
     }
 
     buildTypes {
